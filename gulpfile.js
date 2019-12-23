338693bd5e3e078378036acc58dc465b3f0487e4
lichenglong <554675188@qq.com>
2019-12-23 20:43:08 +0800

    添加gulp打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 /*
   Slide.js 构建
  */
+const gulp = require("gulp");
+const uglify = require('gulp-uglify');
+const minify = require('gulp-minify-css');
+
+gulp.task('build',function(){
+    gulp.src('./src/slide.js').pipe(uglify()).pipe(gulp.dest('./dist'));
+});
