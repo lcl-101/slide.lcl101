0cd8e4d2e44929a8f4182f09a00691379a463b4a
lichenglong <554675188@qq.com>
2019-12-24 12:21:56 +0800

    js,css压缩

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,32 @@
 const gulp = require("gulp");
 const uglify = require('gulp-uglify');
 const minify = require('gulp-minify-css');
+const plugins = require('gulp-load-plugins')();
 
-gulp.task('build',function(){
-    gulp.src('./src/slide.js').pipe(uglify()).pipe(gulp.dest('./dist'));
+/**
+ * build:css
+ */
+gulp.task('build:css',function(){
+    gulp.src('./src/css/*.css')
+        .pipe(minify({compatibility: 'ie7'}))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+/**
+ * build:js
+ */
+gulp.task('build:js',function(){
+    gulp.src('./src/slide.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+});
+
+/**
+ *
+ */
+gulp.task('default',function(callback){
+    plugins.sequence(
+        "build:css",
+        "build:js"
+    )(callback);
 });
